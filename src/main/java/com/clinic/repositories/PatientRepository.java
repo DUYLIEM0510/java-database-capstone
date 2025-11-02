1f089d9efb507083701f32fc3188c188ddbd0d7b
DUYLIEM0510 <phamduyliem0510@gmail.com>
2025-11-02 20:59:17 +0700

    Create PatientRepository interface for patient data

---
+++ src/main/java/com/clinic/repositories/PatientRepository.java
@@ -0,0 +1,10 @@
+package com.clinic.repositories;
+
+import com.clinic.models.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+    Patient findByUserEmail(String email);
+}

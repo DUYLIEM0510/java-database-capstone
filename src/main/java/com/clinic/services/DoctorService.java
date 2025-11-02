4dfc6abf3c224a230fa32ba86c092c7c22781ede
DUYLIEM0510 <phamduyliem0510@gmail.com>
2025-11-02 21:00:26 +0700

    Implement DoctorService with basic CRUD methods

---
+++ src/main/java/com/clinic/services/DoctorService.java
@@ -0,0 +1,23 @@
+package com.clinic.services;
+
+import com.clinic.models.Doctor;
+import com.clinic.repositories.DoctorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DoctorService {
+
+    @Autowired
+    private DoctorRepository doctorRepository;
+
+    public List<Doctor> getAllDoctors() {
+        return doctorRepository.findAll();
+    }
+
+    public Doctor saveDoctor(Doctor doctor) {
+        return doctorRepository.save(doctor);
+    }
+}
